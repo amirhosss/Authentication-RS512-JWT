99fe9496b716fb9500de7ac804f881e67ac60cb7
Amirhossein <amirhosseinleo@gmail.com>
2021-09-20 17:12:35 +0430

    Bugs fixed

--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,22 @@
+import uuid
+from fastapi import Form
 from pydantic import BaseModel, constr
 from typing import Optional
+
+
+class UserInput(BaseModel):
+    first_name: str = Form(..., max_length=50)
+    last_name: str = Form(..., max_length=50)
+    username: str = Form(..., max_length=50)
+    password: str = Form(
+        ...,
+        regex=r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,50}$'
+    )
+    email: str = Form(
+        ...,
+        max_length=64,
+        regex=r'^([a-zA-Z0-9]+(?:[.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:[.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,7})$'
+    )
 
 
 class UserInputDb(BaseModel):
